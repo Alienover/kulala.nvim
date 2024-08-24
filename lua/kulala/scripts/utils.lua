7987b7cfe9afff0e29022762da6856418d6cb003
Jiarong <alienover.l.d@gmail.com>
2024-08-23 22:46:09 -0700

    fix(scripts): support multi args in clearing global vars

--- lua/kulala/scripts/utils.lua
+++ lua/kulala/scripts/utils.lua
@@ -1,18 +1,29 @@
 local Fs = require("kulala.utils.fs")
 local M = {}
 
-M.clear_global = function(key_or_keys)
+M.clear_global = function(...)
   local globals_fp = Fs.get_global_scripts_variables_file_path()
-  local globals = Fs.file_exists(globals_fp) and vim.fn.json_decode(Fs.read_file(globals_fp)) or {}
-  if key_or_keys == nil then
-    globals = {}
-  elseif type(key_or_keys) == "table" then
-    for _, key in ipairs(key_or_keys) do
+  local globals = Fs.file_exists(globals_fp)
+      and vim.fn.json_decode(Fs.read_file(globals_fp))
+    or {}
+
+  local keys_length = select("#", ...)
+
+  if keys_length > 0 then
+    local keys = {}
+
+    for i = 1, select("#", ...) do
+      local item = select(i, ...)
+      table.insert(keys, item)
+    end
+
+    for _, key in ipairs(vim.iter(keys):flatten():totable()) do
       globals[key] = nil
     end
-  elseif type(key_or_keys) == "string" then
-    globals[key_or_keys] = nil
+  else
+    globals = {}
   end
+
   Fs.write_file(globals_fp, vim.fn.json_encode(globals))
 end
 
